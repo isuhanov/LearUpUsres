7a836694581231da818b380d70eba73ea5699bdf
tuc-nado <signat1911@gmail.com>
2022-04-24 20:16:42 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lu-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'learnupusers';
+}
